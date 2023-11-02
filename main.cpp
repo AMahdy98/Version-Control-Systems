5cb8fdf66cb9d1a35c85e744c8ca8b390b149a23
AMahdy98 <ahmadmahdy3098@gmail.com>
2023-11-02 19:05:41 +0200

    add minimum feature

--- main.cpp
+++ main.cpp
@@ -11,11 +11,22 @@ int getSum(std::vector<int> numbers)
     return sum;
 }
 
+int getMin(std::vector<int> numbers)
+{
+    int min = numbers[0];
+    for (auto num : numbers)
+    {
+        min = min > num ? num : min;
+    }
+    return min;
+}
+
 int main()
 {
 
     std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::cout << getSum(nums) << std::endl;
+    std::cout << "Sum: " << getSum(nums) << std::endl;
+    std::cout << "Min: " << getMin(nums) << std::endl;
 
     return 0;
 }
